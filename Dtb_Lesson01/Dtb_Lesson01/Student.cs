ef8734f32865ff2dc2647e536e2259eead1605d4
Diulin26 <ledieulinh3011@gmail.com>
2026-08-15 11:48:38 +0700

    Thêm bài tập Lesson 01

---
+++ Dtb_Lesson01/Dtb_Lesson01/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtb_Lesson01
+{
+    internal class Student
+    {
+        public string mssv { get; set; }
+        public string name { get; set; }
+        public DateTime? Ngaysinh { get; set; }
+
+        public string GioiTinh { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public string NganhHoc { get; set; }
+        public double DiemTrungBinh { get; set; }
+        public string TrangThaiHocTap { get; set; }
+    }
+}
